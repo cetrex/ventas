21fd25bbb1232067f534187194519fe889512e8b
cetrex <cetrex@gmail.com>
2022-03-09 12:31:04 -0600

    inicializar variables 12:30

--- instance.tf
+++ instance.tf
@@ -7,8 +7,15 @@ terraform {
     }
 }
 
+variable "AWS_ACCESS_KEY_ID" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
     region = "us-east-2"
+    
 }
 
 resource "aws_instance" "hello-instance"{
